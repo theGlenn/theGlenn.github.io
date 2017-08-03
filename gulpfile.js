93c0d23393c3fea1947bc3c21842ea801d933d99
Glenn Sonna <kenpachi.os@gmail.com>
2017-08-03 13:30:38 +0200

    up

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const useref = require('gulp-useref');
+const uglify = require('gulp-uglify');
+const cssnano = require('gulp-cssnano');
+const gulpIf = require('gulp-if');
+const imagemin = require('gulp-imagemin');
+const runSequence = require('run-sequence');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+  })
+});
+
+gulp.task('sass', function() {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('useref', function(){
+  return gulp.src('src/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+
+
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+});
+
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('src/scss/**/*.scss', ['sass']); 
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('src/*.html', browserSync.reload); 
+  gulp.watch('src/js/**/*.js', browserSync.reload); 
+});
+
+gulp.task('default', function (callback) {
+  runSequence(['sass','browserSync', 'watch'],
+    callback
+  )
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', 
+    ['sass', 'useref', 'fonts'],
+    callback
+  )
+});
+
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+function main(params) {
+  console.log('lol', params);
+}
