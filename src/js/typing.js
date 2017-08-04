bfa7b41784035147b0e9b4b54ef503e87895cba8
Glenn Sonna <kenpachi.os@gmail.com>
2017-08-04 12:18:02 +0200

    up

--- src/js/typing.js
+++ src/js/typing.js
@@ -54,6 +54,6 @@ window.onload = function() {
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #2176FF }";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #fff }";
   document.body.appendChild(css);
 };
